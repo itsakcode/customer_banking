aa70e2408244c61961c69b10758d64c640ac9e40
itsakcode <appsbyajay@gmail.com>
2023-11-11 20:46:53 -0500

    Updated line length to align the output

--- customer_banking.py
+++ customer_banking.py
@@ -10,9 +10,10 @@ def main():
     It displays the interest earned on the savings and CD accounts and updates the balances.
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
-    print("-" * 40)
-    print(f"{'***** Welcome to AK Bank *****':^40}")
-    print("-" * 40)
+    line_len = 70
+    print("-" * line_len)
+    print(f"{'***** Welcome to AK Bank *****':^70}")
+    print("-" * line_len)
    
     while True:
         print(f"\n Select an account to check balance and interest earned.\n")
@@ -25,7 +26,7 @@ def main():
                 s_balance = utils.getFloatInput("Balance: ")
                 s_int = utils.getFloatInput("Interest Rate: ")
                 s_months = utils.getIntInput("Months: ")
-                print("-" * 40)
+                print("-" * line_len)
 
                 # Call the create_savings_account function and pass the variables from the user.
                 updated_savings_balance, s_interest_earned = create_savings_account(s_balance, s_int, s_months)
@@ -35,12 +36,12 @@ def main():
                     f"Interest Earned for {s_months} months is {s_interest_earned:,.2f} at the rate of {s_int}%.\n")
             case '2':
                 # Prompt the user to set the CD balance, interest rate, and months for the CD account.
-                print("-" * 40)
+                print("-" * line_len)
                 print(f"\nEnter your Certificate of Deposit Details.\n")
                 cd_balance = utils.getFloatInput("Balance: ")
                 cd_int = utils.getFloatInput("Interest Rate: ")
                 cd_maturity = utils.getIntInput("Months: ")
-                print("-" * 40)
+                print("-" * line_len)
 
                 # Call the create_cd_account function and pass the variables from the user.
                 updated_cd_balance, cd_interest_earned = create_cd_account(cd_balance, cd_int, cd_maturity)
